7b7bcd28ef6313bd646224632acc7f67988619ec
Yannis <yannis.le-bars@etu.univ-nantes.fr>
2017-11-13 13:39:49 +0100

    Ajout droit.sql

---
+++ droit.sql
@@ -0,0 +1,20 @@
+GRANT SELECT ON Table_Faits TO admi42;
+GRANT SELECT ON Motifs TO admi42;
+GRANT SELECT ON Temps TO admi42;
+GRANT SELECT ON Orga TO admi42;
+GRANT SELECT ON Metiers_Cibles TO admi42;
+GRANT SELECT ON Nb_Travailleurs TO admi42;
+
+GRANT SELECT ON Table_Faits TO admi21;
+GRANT SELECT ON Motifs TO admi21;
+GRANT SELECT ON Temps TO admi21;
+GRANT SELECT ON Orga TO admi21;
+GRANT SELECT ON Metiers_Cibles TO admi21;
+GRANT SELECT ON Nb_Travailleurs TO admi21;
+
+GRANT SELECT ON Table_Faits TO admi45;
+GRANT SELECT ON Motifs TO admi45;
+GRANT SELECT ON Temps TO admi45;
+GRANT SELECT ON Orga TO admi45;
+GRANT SELECT ON Metiers_Cibles TO admi45;
+GRANT SELECT ON Nb_Travailleurs TO admi45;
