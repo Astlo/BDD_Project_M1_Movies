62fc1f20e60fc9779fa78c60d99b393f33a26ff8
Guillaume FORTIN <E139198C@I019V7pc09.ensinfo.sciences.univ-nantes.prive>
2017-11-14 17:13:33 +0100

    maj requtes

--- requetes.sql
+++ requetes.sql
@@ -1,5 +1,8 @@
 --@requetes.sql
 
 --GROUP BY : la moyenne des notes utilisateurs pour le tag ?
-SELECT AVG(rating), userTag
-FROM Rating
+SELECT AVG(rating) as moyenne, userTag
+FROM (Rating JOIN Tagging ON (Rating.userId = Tagging.userId and Rating.movieId = Tagging.movieId)) NATURAL JOIN Movies
+GROUP BY (userTag)
+ORDER BY (moyenne)
+LIMIT 10;
