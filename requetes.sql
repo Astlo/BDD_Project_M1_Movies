fee8410c86d5b4ff41f5708af86dc9c73aab2613
Clement TRAVERS <E135973X@I019V7pc07.ensinfo.sciences.univ-nantes.prive>
2017-11-14 19:04:36 +0100

    Requetes

--- requetes.sql
+++ requetes.sql
@@ -5,5 +5,23 @@ SELECT AVG(rating), userTag
 FROM Rating
 
 
-SELECT movieProduction, movieTitle OVER (PARTITION BY movieProduction)
-FROM Movies
+-- Somme de l'argent du box office de tous les films pour chaque année
+SELECT annee, SUM(movieBoxOffice) AS boxOffice
+FROM Movies NATURAL JOIN Temps NATURAL JOIN Rating
+WHERE movieBoxOffice IS NOT NULL
+GROUP BY GROUPING SETS (annee);
+
+
+-- Les 10 films ayant reçus les meilleurs ratings, par rapport à leur rang dans le box office
+SELECT movieTitle, rating, RANK() over (ORDER BY movieBoxOffice DESC) AS rangBoxOffice
+FROM Movies NATURAL JOIN Rating
+WHERE rownum <= 10
+ORDER BY rating DESC;
+
+--Les 20 meilleurs succès au box office triés par les producteurs (partition by)
+SELECT movieProduction, movieTitle, annee, movieBoxOffice, rank() OVER (PARTITION BY movieProduction ORDER BY movieBoxOffice DESC) AS rank
+FROM Movies NATURAL JOIN Temps NATURAL JOIN Rating
+WHERE rownum <= 20;
+
+--Le film ayant reçu la meilleure notation pour chaque année
+SELECT movieTitle, annee 
