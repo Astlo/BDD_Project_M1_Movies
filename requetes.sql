1a4b7138156d1f158f937ae2568c863d404169f1
Clement TRAVERS <E135973X@I019V7pc07.ensinfo.sciences.univ-nantes.prive>
2017-11-14 17:17:16 +0100

    modification

--- requetes.sql
+++ requetes.sql
@@ -3,3 +3,7 @@
 --GROUP BY : la moyenne des notes utilisateurs pour le tag ?
 SELECT AVG(rating), userTag
 FROM Rating
+
+
+SELECT movieProduction, movieTitle OVER (PARTITION BY movieProduction)
+FROM Movies
