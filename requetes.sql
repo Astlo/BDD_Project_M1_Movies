1d199d26ffa1f1ad9c86f9953a1cafa1a5ace795
ClementTravers <clement.travers@etu.univ-nantes.fr>
2017-11-14 19:24:24 +0100

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -1,6 +1,6 @@
 --@requetes.sql
 
---GROUP BY : la moyenne des notes utilisateurs pour le tag ?
+--GROUP BY : la moyenne des notes utilisateurs pour le tag
 SELECT AVG(rating) as moyenne, userTag
 FROM (Rating JOIN Tagging ON (Rating.userId = Tagging.userId and Rating.movieId = Tagging.movieId)) NATURAL JOIN Movies
 GROUP BY (userTag)
@@ -10,3 +10,27 @@ ORDER BY (moyenne);
 SELECT movieDVD, count(movieId)
 FROM Movies
 GROUP BY CUBE (movieDVD,movieCountry);
+
+-- Somme de l'argent du box office de tous les films pour chaque année
+SELECT annee, SUM(movieBoxOffice) AS boxOffice
+FROM Movies NATURAL JOIN Rating NATURAL JOIN Temps
+WHERE movieBoxOffice IS NOT NULL
+GROUP BY GROUPING SETS (annee);
+
+
+-- Les 10 films ayant reçus les meilleurs ratings, par rapport à leur rang dans le box office
+SELECT movieTitle, rating, RANK() over (ORDER BY movieBoxOffice DESC) AS rangBoxOffice
+FROM Movies NATURAL JOIN Rating
+WHERE rownum <= 10
+ORDER BY rating DESC;
+
+--Les 20 meilleurs succès au box office triés par les producteurs (partition by)
+SELECT movieProduction, movieTitle, annee, movieBoxOffice, rank() OVER (PARTITION BY movieProduction ORDER BY movieBoxOffice DESC) AS rank
+FROM Movies NATURAL JOIN Rating NATURAL JOIN Temps
+WHERE rownum <= 20;
+
+--Le film ayant reçu la meilleure notation pour chaque année
+SELECT movieTitle, annee, MAX(rating)
+FROM Movies NATURAL JOIN Rating NATURAL JOIN Temps
+WHERE annee IS NOT NULL
+GROUP BY GROUPING SETS (annee);
